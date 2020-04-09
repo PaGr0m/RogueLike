e68a75c8340363292f84b921c9638f508a5fbc6a
Pavel Gromov <pagrom27@gmail.com>
2020-04-09 20:35:54 +0300

    Test graphics library

---
+++ src/test/java/graphics/JexerExample.java
@@ -0,0 +1,18 @@
+package graphics;
+import jexer.TApplication;
+
+public class JexerExample extends TApplication {
+
+    public JexerExample() throws Exception {
+        super(BackendType.XTERM);
+
+        addToolMenu();
+        addFileMenu();
+        addWindowMenu();
+    }
+
+    public static void main(String [] args) throws Exception {
+        JexerExample app = new JexerExample();
+        app.run();
+    }
+}
