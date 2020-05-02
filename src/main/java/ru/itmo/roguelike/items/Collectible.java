06819a0af8fa896a9cbf68023c4e5cfd431e4c3b
Ellen Kartysheva <el.kartysheva@yandex.ru>
2020-05-02 18:52:14 +0300

    Add draw to collectable

--- src/main/java/ru/itmo/roguelike/items/Collectible.java
+++ src/main/java/ru/itmo/roguelike/items/Collectible.java
@@ -1,11 +1,18 @@
 package ru.itmo.roguelike.items;
 import ru.itmo.roguelike.Collidable;
-import ru.itmo.roguelike.Drawable;
+import ru.itmo.roguelike.render.drawable.Drawable;
 
-public abstract class Collectible implements Drawable, Collidable {
+import java.awt.*;
+
+public abstract class Collectible extends Drawable implements Collidable {
 
     BonusType bonus;
     int bonusSize;
+    protected int positionX;
+    protected int positionY;
+    private int width;
+    private int height;
+    private Color color;
 
     @Override
     public void collide(Collidable c) {
--- src/main/java/ru/itmo/roguelike/items/MedKitBig.java
+++ src/main/java/ru/itmo/roguelike/items/MedKitBig.java
@@ -1,13 +1,24 @@
 package ru.itmo.roguelike.items;
 
+import ru.itmo.roguelike.render.drawable.DrawableDescriptor;
+
+import java.awt.*;
+
 import static ru.itmo.roguelike.items.BonusType.HP;
 
 public class MedKitBig extends Collectible{
     BonusType bonusType = HP;
     int bonusSize = 75;
+    private final int width = 10;
+    private final int height = 10;
+    private final Color color = Color.RED;
+
+    {
+        drawableDescriptor.setColor(color);
+    }
 
     @Override
     public void draw() {
-
+        drawableDescriptor.setX(this.positionX).setY(this.positionY);
     }
 }
