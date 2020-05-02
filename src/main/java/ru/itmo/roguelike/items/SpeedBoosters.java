fe7675bcfc076e05c12b2f799b64c309b4980b11
Ellen Kartysheva <el.kartysheva@yandex.ru>
2020-05-02 19:19:35 +0300

    Aff fix to collectible classes

--- src/main/java/ru/itmo/roguelike/items/SpeedBoosters.java
+++ src/main/java/ru/itmo/roguelike/items/SpeedBoosters.java
@@ -7,9 +7,14 @@ import static ru.itmo.roguelike.items.BonusType.SPEED;
 public class SpeedBoosters extends Collectible{
     BonusType bonusType = SPEED;
     int bonusSize = 25;
-    private final int width = 5;
-    private final int height = 5;
-    private final Color color = Color.RED;
+
+    {
+        drawableDescriptor.setColor(color);
+        width = 10;
+        height = 10;
+        color = Color.RED;
+    }
+
 
     @Override
     public void draw() {
