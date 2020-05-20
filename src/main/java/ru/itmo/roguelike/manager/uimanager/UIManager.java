4cc94ec14fbb819854df3b158c39bd283ae062ef
Pavel Gromov <pagrom27@gmail.com>
2020-05-21 00:03:00 +0300

    Some fix

--- src/main/java/ru/itmo/roguelike/manager/uimanager/UIManager.java
+++ src/main/java/ru/itmo/roguelike/manager/uimanager/UIManager.java
@@ -7,14 +7,17 @@ public class UIManager {
     public UIManager() {}
 
     public static void addStatusBar(Graphics graphics) {
-        int width = 10;
+        // FIXME: magic numbers
         int height = 30;
+        int width = 10;
+        int delta = 20;
+
         graphics.setFont(new Font("Courier New", Font.BOLD, 24));
         graphics.setColor(Color.ORANGE);
-        graphics.drawString("Status", width, 30);
+        graphics.drawString("Status", width, height);
 
         graphics.setFont(new Font("Courier New", Font.BOLD, 12));
-        graphics.drawString("HP: ", width , height+= 20);
-        graphics.drawString("MP: ", width , height+= 20);
+        graphics.drawString("HP: ", width, height += delta);
+        graphics.drawString("MP: ", width, height += delta);
     }
 }
