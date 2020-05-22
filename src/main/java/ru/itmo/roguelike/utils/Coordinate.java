03e82cf8979331b2ad472f24993b448d857d55e7
Ellen Kartysheva <el.kartysheva@yandex.ru>
2020-05-22 20:42:47 +0300

    Add Coordinate

---
+++ src/main/java/ru/itmo/roguelike/utils/Coordinate.java
@@ -0,0 +1,27 @@
+package ru.itmo.roguelike.utils;
+
+public class Coordinate {
+    private int posX;
+    private int posY;
+
+    public Coordinate(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public int getX() {
+        return posX;
+    }
+
+    public void setX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getY() {
+        return posY;
+    }
+
+    public void setY(int posY) {
+        this.posY = posY;
+    }
+}
