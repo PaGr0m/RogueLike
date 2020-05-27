0106c93ce503338b3ffe2f0ed2317574da3c8457
Alexander Chori <alexandrchori@gmail.com>
2020-05-27 17:10:38 +0300

    Optimize imports

--- src/main/java/ru/itmo/roguelike/characters/mobs/strategy/PassiveBehavior.java
+++ src/main/java/ru/itmo/roguelike/characters/mobs/strategy/PassiveBehavior.java
@@ -1,7 +1,6 @@
 package ru.itmo.roguelike.characters.mobs.strategy;
 
 import ru.itmo.roguelike.utils.IntCoordinate;
-import ru.itmo.roguelike.utils.Pair;
 
 public class PassiveBehavior implements MobBehavior {
     @Override
