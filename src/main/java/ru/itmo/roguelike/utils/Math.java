fef7769edba542db1da3d51aa930987caddc517e
Ellen Kartysheva <el.kartysheva@yandex.ru>
2020-05-28 14:08:37 +0300

    Add comment

--- src/main/java/ru/itmo/roguelike/utils/Math.java
+++ src/main/java/ru/itmo/roguelike/utils/Math.java
@@ -1,6 +1,13 @@
 package ru.itmo.roguelike.utils;
 
 public class Math {
+    /**
+     * Return random double in specified boundaries
+     *
+     * @param minBound
+     * @param maxBound
+     * @return random number in [minBound, maxBound]
+     */
     public static double getRandomNumber(double minBound, double maxBound) {
         return java.lang.Math.random() * (maxBound - minBound) + minBound;
     }
