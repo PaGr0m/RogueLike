53659d176063798083649a12c0b12cc8766493fe
Pavel Gromov <pagrom27@gmail.com>
2020-05-29 00:04:12 +0300

    Add status exp and level

--- src/main/java/ru/itmo/roguelike/manager/uimanager/UIManager.java
+++ src/main/java/ru/itmo/roguelike/manager/uimanager/UIManager.java
@@ -3,10 +3,13 @@ package ru.itmo.roguelike.manager.uimanager;
 import org.jetbrains.annotations.NotNull;
 import ru.itmo.roguelike.characters.Player;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import java.awt.*;
 import java.awt.font.TextLayout;
 import java.awt.geom.AffineTransform;
 
+@Singleton
 public class UIManager {
 
     private final static Font MAIN_TEXT_FONT = new Font(Font.SANS_SERIF, Font.BOLD, 35);
@@ -15,10 +18,14 @@ public class UIManager {
     private final static Stroke MAIN_TEXT_STROKE = new BasicStroke(4f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
     private final static Stroke SECONDARY_TEXT_STROKE = new BasicStroke(2f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
 
-    public UIManager() {
+    private final Player player;
+
+    @Inject
+    public UIManager(Player player) {
+        this.player = player;
     }
 
-    public static void addStatusBar(@NotNull Graphics2D graphics) {
+    public void addStatusBar(@NotNull Graphics2D graphics) {
         // FIXME: magic numbers
         int height = 30;
         int width = 10;
@@ -29,12 +36,17 @@ public class UIManager {
 
         TextLayout statusTL = new TextLayout("Status", MAIN_TEXT_FONT, graphics.getFontRenderContext());
         TextLayout hpTL = new TextLayout(
-                String.format("HP: %d"),
+                String.format("HP: %d", player.getHp()),
                 SECONDARY_TEXT_FONT,
                 graphics.getFontRenderContext()
         );
-        TextLayout attackTL = new TextLayout(
-                "Attack: ",
+        TextLayout levelTl = new TextLayout(
+                String.format("Level: %d", player.getLevel()),
+                SECONDARY_TEXT_FONT,
+                graphics.getFontRenderContext()
+        );
+        TextLayout expTl = new TextLayout(
+                String.format("Experience: %.2f", player.getExp()),
                 SECONDARY_TEXT_FONT,
                 graphics.getFontRenderContext()
         );
@@ -46,11 +58,15 @@ public class UIManager {
         graphics.setStroke(SECONDARY_TEXT_STROKE);
         graphics.draw(hpTL.getOutline(transform));
         transform.translate(0, delta);
-        graphics.draw(attackTL.getOutline(transform));
+        graphics.draw(levelTl.getOutline(transform));
+        transform.translate(0, delta);
+        graphics.draw(expTl.getOutline(transform));
+
 
         graphics.setColor(Color.WHITE);
         statusTL.draw(graphics, width, height);
         hpTL.draw(graphics, width, height + delta);
-        attackTL.draw(graphics, width, height + 2 * delta);
+        levelTl.draw(graphics, width, height + 2 * delta);
+        expTl.draw(graphics, width, height + 3 * delta);
     }
 }
