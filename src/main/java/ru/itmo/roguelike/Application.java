c7d4c522955761aa68beafc43ed1707878c92542
Eugene Kravchenko <zzzheka97@gmail.com>
2020-06-05 15:37:54 +0300

    add map file chooser

--- src/main/java/ru/itmo/roguelike/Application.java
+++ src/main/java/ru/itmo/roguelike/Application.java
@@ -3,20 +3,24 @@ package ru.itmo.roguelike;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import ru.itmo.roguelike.ioc.IOModule;
 import ru.itmo.roguelike.ioc.ManagersModule;
 import ru.itmo.roguelike.ioc.RenderModule;
 import ru.itmo.roguelike.manager.gamemanager.GameManager;
 import ru.itmo.roguelike.settings.GameSettings;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import java.io.File;
 import java.util.concurrent.*;
+
+import static javax.swing.JFileChooser.APPROVE_OPTION;
+import static javax.swing.JFileChooser.FILES_ONLY;
 
 public class Application {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            GameSettings.FILENAME = args[0];
-        }
-
+        GameSettings.FILENAME = chooseMapFileName();
         Application application = new Application();
         application.run();
         System.exit(0);
@@ -39,6 +43,29 @@ public class Application {
                 e.printStackTrace();
             }
         }
+    }
+
+    public static @Nullable String chooseMapFileName() {
+        JFileChooser fc = new JFileChooser(System.getProperty("user.dir"));
+        fc.setFileFilter(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return file.isDirectory() || file.getName().endsWith(".mapfile");
+            }
+
+            @Override
+            public String getDescription() {
+                return "Map files";
+            }
+        });
+
+        fc.setFileSelectionMode(FILES_ONLY);
+        fc.setApproveButtonText("LOAD");
+
+        if (APPROVE_OPTION == fc.showOpenDialog(null)) {
+            return fc.getSelectedFile().getAbsolutePath();
+        }
+        return null;
     }
 
     private void rescheduleGameLoop(
